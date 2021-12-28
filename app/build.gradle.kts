9b5806fb52ce1bb7fb7375e1114a2f64ac3f4afb
Lee WonJoong <realjoong@gmail.com>
2021-12-28 11:59:23 +0900

    Init Project

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+}
+
+android {
+    defaultConfig {
+        applicationId = "com.ivyclub.contact"
+        versionCode = Apps.versionCode
+        versionName = Apps.versionName
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+    implementation(Dep.AndroidX.core)
+    implementation(Dep.AndroidX.appcompat)
+    implementation(Dep.AndroidX.material)
+    implementation(Dep.AndroidX.constraintLayout)
+    testImplementation(Dep.Test.jUnit)
+    androidTestImplementation(Dep.Test.ext)
+    androidTestImplementation(Dep.Test.espresso)
+}
