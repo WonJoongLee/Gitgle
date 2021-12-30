63116c6ee819918d5a74a945b61bb8d281f71edc
Lee WonJoong <realjoong@gmail.com>
2021-12-30 20:58:21 +0900

    state flow로 로딩 상태 구현

--- features/search/src/main/java/com/wonjoong/search/ui/SearchViewModel.kt
+++ features/search/src/main/java/com/wonjoong/search/ui/SearchViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wonjoong.domain.usecase.user.GetUserInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -18,10 +19,16 @@ class SearchViewModel @Inject constructor(
     val follower = MutableLiveData<String>()
     val following = MutableLiveData<String>()
     val createdAt = MutableLiveData<String>()
+    val isLoading = MutableStateFlow(false)
+    val dataCategoryVisibility = MutableStateFlow(false)
 
     fun searchInput() {
+        isLoading.value = true
+        dataCategoryVisibility.value = false
         viewModelScope.launch {
             val result = getUserInfo.execute(userInput.value.toString())
+            isLoading.value = false
+            dataCategoryVisibility.value = true
             profileImageUrl.value = result.profileUrl
             name.value = result.name
             follower.value = result.followers
