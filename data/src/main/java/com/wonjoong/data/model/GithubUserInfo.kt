351f0662e2ca685c63fcac025dbe79cafb270c4a
Lee WonJoong <realjoong@gmail.com>
2021-12-30 23:16:33 +0900

    유저 검색했는데 안나왔을 때 상태 처리

--- data/src/main/java/com/wonjoong/data/model/GithubUserInfo.kt
+++ data/src/main/java/com/wonjoong/data/model/GithubUserInfo.kt
@@ -3,11 +3,14 @@ package com.wonjoong.data.model
 import com.google.gson.annotations.SerializedName
 
 data class GithubUserInfo(
-    val name: String,
+    val name: String?,
     @SerializedName("avatar_url")
-    val profileUrl: String,
-    val followers: String,
-    val following: String,
+    val profileUrl: String?,
+    val followers: String?,
+    val following: String?,
     @SerializedName("created_at")
-    val createdAt: String
+    val createdAt: String?,
+    // If there is no target user(or repository),
+    // this message will be "Not Found"
+    val message: String?
 )
--- features/search/src/main/java/com/wonjoong/search/ui/SearchViewModel.kt
+++ features/search/src/main/java/com/wonjoong/search/ui/SearchViewModel.kt
@@ -3,12 +3,14 @@ package com.wonjoong.search.ui
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.wonjoong.data.model.GithubUserInfo
 import com.wonjoong.domain.usecase.user.GetUserInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+// todo UIState(StateFlow)로 업데이트하기
 @HiltViewModel
 class SearchViewModel @Inject constructor(
     private val getUserInfo: GetUserInfoUseCase
@@ -21,19 +23,32 @@ class SearchViewModel @Inject constructor(
     val createdAt = MutableLiveData<String>()
     val isLoading = MutableStateFlow(false)
     val dataCategoryVisibility = MutableStateFlow(false)
+    val isUserRepoNotFound = MutableStateFlow(false)
 
     fun searchInput() {
         isLoading.value = true
         dataCategoryVisibility.value = false
         viewModelScope.launch {
-            val result = getUserInfo.execute(userInput.value.toString())
-            isLoading.value = false
-            dataCategoryVisibility.value = true
-            profileImageUrl.value = result.profileUrl
-            name.value = result.name
-            follower.value = result.followers
-            following.value = result.following
-            createdAt.value = result.createdAt.substringBefore("T")
+            runCatching {
+                getUserInfo.execute(userInput.value.toString())
+            }.onSuccess { githubUserInfo ->
+                isLoading.value = false
+                bindResultData(githubUserInfo)
+            }.onFailure {
+                isLoading.value = false
+                isUserRepoNotFound.value = true
+            }
         }
+    }
+
+    private fun bindResultData(githubUserInfo: GithubUserInfo) {
+        isUserRepoNotFound.value = false
+        isLoading.value = false
+        dataCategoryVisibility.value = true
+        profileImageUrl.value = githubUserInfo.profileUrl ?: "-"
+        name.value = githubUserInfo.name ?: "-"
+        follower.value = githubUserInfo.followers ?: "-"
+        following.value = githubUserInfo.following ?: "-"
+        createdAt.value = githubUserInfo.createdAt?.substringBefore("T") ?: "-"
     }
 }
