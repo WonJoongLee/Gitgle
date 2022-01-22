9e430791d12ebe2d91641f9118df269952c31a4f
Lee WonJoong <realjoong@gmail.com>
2022-01-22 12:22:43 +0900

    favorite에 compose 의존성 추가

--- features/favorite/build.gradle.kts
+++ features/favorite/build.gradle.kts
@@ -9,15 +9,21 @@ plugins {
 android {
     buildFeatures {
         dataBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = Versions.compose
     }
 }
 
 dependencies {
     Dep.baseImplementation.forEach(::implementation)
+    Dep.compose.forEach(::implementation)
     implementation(project(":shared"))
     implementation(project(":domain"))
     kapt(Dep.Libs.hiltCompiler)
     testImplementation(Dep.Test.jUnit)
     androidTestImplementation(Dep.Test.ext)
     androidTestImplementation(Dep.Test.espresso)
+    androidTestImplementation(Dep.Compose.uiTestJUnit4)
 }
